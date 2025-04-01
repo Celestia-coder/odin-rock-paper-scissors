dac886b0c23f1a04c08a036e4efc3785ee9689e3
Celestia-coder <141723734+Celestia-coder@users.noreply.github.com>
2025-04-02 00:05:59 +0800

    Add rules of the game

---
+++ javascript.js
@@ -0,0 +1,19 @@
+/*
+    ROCK PAPER SCISSORS GAME
+
+    Rules:
+    - Rock beats scissors 
+    - Scissors beat paper
+    - Paper beats rock
+*/
+
+// Get computer choice
+function getComputerChoice () {
+
+}
+
+// Get human choice
+
+// Keep track the score
+
+// Play the game
