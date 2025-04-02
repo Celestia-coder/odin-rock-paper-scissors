55752f1ef1e38d562d33858115eee3c4fa148258
Celestia-coder <141723734+Celestia-coder@users.noreply.github.com>
2025-04-02 22:55:57 +0800

    Add playRound function

--- javascript.js
+++ javascript.js
@@ -7,29 +7,86 @@
     - Paper beats rock
 */
 
+// Scores
+let humanScore = 0;
+let computerScore = 0;
+
+
 // Get computer choice
 function getComputerChoice () {
     let computerChoice = Math.floor(Math.random() * 3);
 
     if (computerChoice == 0) {
-        return console.log("rock");
+        return "rock";
     } else if (computerChoice == 1) {
-        return console.log("scissors");
+        return "scissors";
     } else if (computerChoice == 2) {
-        return console.log("paper");
+        return "paper";
     }
 }
-
-getComputerChoice();
 
 // Get human choice
 function getHumanChoice() {
     let humanChoice = prompt("Rock, Paper, or Scissors?");
-    return humanChoice;
+    return humanChoice.toLowerCase();
 }
 
-console.log(getHumanChoice());
 
-// Keep track the score
+let result = ""; // Store the result of the game (Win / Lose)
 
 // Play the game
+function playRound(humanChoice, computerChoice) {
+
+    // Print computer and human selected choice
+    console.log("Computer: " + computerChoice);
+    console.log("Human: " + humanChoice);
+
+    if (humanChoice == computerChoice) {
+        result = "DRAW!"; // Both pick the same choice
+    } else {
+        switch(humanChoice) {
+            case "rock":
+                if (computerChoice == "paper") {
+                    computerScore++;
+                    result = "LOSE! Paper beats rock";
+                } else {
+                    humanScore++;
+                    result = "WIN! Rock beats scissors";
+                }
+                break;
+
+            case "paper":
+                if (computerChoice == "scissors") {
+                    computerScore++;
+                    result = "LOSE! Scissors beats paper";
+                } else {
+                    humanScore++;
+                    result = "WIN! Paper beats rock";
+                }
+                break;
+
+            case "scissors":
+                if (computerChoice == "rock") {
+                    computerScore++;
+                    result = "LOSE! Rock beats scissors";
+                } else {
+                    humanScore++;
+                    result = "WIN! Scissors beats paper";
+                }
+                break;
+        }
+    }
+
+    return result;
+}
+
+// Store choices
+const computerSelection = getComputerChoice();
+const humanSelection = getHumanChoice();
+
+console.log(playRound(humanSelection, computerSelection));
+
+// Print the score
+console.log(`Score:
+    Computer = ${computerScore}
+    Human = ${humanScore}`);
